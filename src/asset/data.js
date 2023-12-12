1d9a484e98a02e7e14538657e7732be3c7a013fa
arick1 <arick@peacockindia.in>
2023-12-12 15:59:45 +0600

    done with partners section

--- src/asset/data.js
+++ src/asset/data.js
@@ -1,3 +1,10 @@
+import allianz from "./image/partners/allianz.png";
+import bro4u from "./image/partners/bro4u.png";
+import ola from "./image/partners/ola.png";
+import pitstop from "./image/partners/pitstop.png";
+import uber from "./image/partners/uber.png";
+import urban from "./image/partners/urban.png";
+
 export const navData = [
     {
         name: "Home",
@@ -15,4 +22,14 @@ export const navData = [
         name: "Login",
         link: "/login"   
     }
+];
+
+
+export const partnersImage = [
+    {image: allianz},
+    {image: bro4u},
+    {image: ola},
+    {image: pitstop},
+    {image: uber},
+    {image: urban},
 ]
--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { partnersImage } from '../asset/data';
 
 const Partners = () => {
   return (
-    <div className='bg-gray-100 w-screen border border-yellow-400 py-8'>
-        <div className='w-[80%] border border-red-600 m-auto text-center'>
-            <h2 className='text-[20px]'>Our Partners</h2>
-            <div className='w-full border border-green-700'>
-                <p>image</p>
+    <div className='bg-gray-100 w-screen  py-8'>
+        <div className='w-[80%]  m-auto text-center'>
+            <h2 className='text-[20px] font-semibold md:text-[23px]'>Our Partners</h2>
+            <div className='w-full flex items-center justify-between mt-3'>
+                {
+                  partnersImage.map(image => {
+                    return(
+                    <div className='w-[10%]'>
+                      <img src={image.image} alt="our partners" />
+                    </div>)
+                  })
+                }
             </div>
         </div>
     </div>
